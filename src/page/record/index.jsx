baa4e79dc8fd6e002788c73275e8d0025d9221b1
jo0hyun <jo0hyun010605@gmail.com>
2024-11-13 15:25:47 +0900

    기능적인 부분 공부해서 간단하게라도 만들어봤습니다.

--- src/page/record/index.jsx
+++ src/page/record/index.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom'; //목록 번호를 가져와서 조회하기 위한 코드
+import index from '../list';
 
-const index = () => {
-  return <div>index</div>;
-};
+function PostViewPage(props) {
+  const navigate = useNavigate();
+  const { postId } = useParams();
+
+  const post = data.find((item) => {
+    return item.id == postId;
+  });
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+    </div>
+  )
+}
 
 export default index;
