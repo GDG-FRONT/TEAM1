155285ede2cb9df85be716b93ecb91f809906ac7
geunpyoPark <rmse303@gmail.com>
2024-11-14 12:37:04 +0900

    메인페이지 완성

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,16 +1,39 @@
-// src/page/home/index.js
 import React from "react";
-import { Link } from "react-router-dom";
+import './home.scss';
+import Group1Image from "./images/Group1.jpg";  // 이미지 파일 import
+import Group2Image from "./images/Group2.jpg";  // 이미지 파일 import
+import Group3Image from "./images/Group3.jpg";  // 이미지 파일 import
+import Group4Image from "./images/Group4.jpg";  // 이미지 파일 import
+import Group5Image from "./images/Group5.jpg";  // 이미지 파일 import
+import TimeImage from "./images/Time.png";      
 
-const Home = () => {
+const Index = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is the first page that loads when you access the app.</p>
-      {/* mypage로 이동하는 링크 추가 */}
-      <Link to="/mypage">Go to My Page</Link>
+    <div className="main-container">
+      <div className="question-section">
+      <span className="greeting">안녕하세요. 근표님.</span>
+        <h2>오늘 하루는<br/> 잘 보내셨나요?</h2>
+        <div className="emoji-buttons">
+          <button className="emoji-button"><img src={Group1Image} alt="" /></button>
+          <button className="emoji-button"><img src={Group2Image} alt="" /></button>
+          <button className="emoji-button"><img src={Group3Image} alt="" /></button>
+          <button className="emoji-button"><img src={Group4Image} alt="" /></button>
+          <button className="emoji-button"><img src={Group5Image} alt="" /></button>
+        </div>
+      </div>
+      <p className="recent-diary-title">최근에 작성한 일기</p>
+      <div className="recent-diary-container">
+        <div className="diary-date">
+          <img src={TimeImage} alt="time" /> 28 May 21
+        </div>
+        <h4 className="diary-title">새로운 직장의 첫 출근</h4>
+        <p className="diary-content">
+          Vivamus ornare metus ut interdum mollis. Donec hendrerit elit at
+          faucibus.
+        </p>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Index;
