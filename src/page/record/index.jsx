323a82631dd5b74c265aab38ae9cbaeca21c2c39
jo0hyun <jo0hyun010605@gmail.com>
2024-11-15 11:19:30 +0900

    css 완성하고 모달창까지 만들었습니다

--- src/page/record/index.jsx
+++ src/page/record/index.jsx
@@ -1,21 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom'; //목록 번호를 가져와서 조회하기 위한 코드
-import index from '../list';
+import React, { useState, useRef } from "react";
+import "./record.scss";
+import { ReactComponent as Arrow } from "./image/arrow.svg";
+import { ReactComponent as Setting } from "./image/setting.svg";
+import { ReactComponent as Correction } from "./image/correction.svg";
+import { ReactComponent as Delete } from "./image/delete.svg";
 
-function PostViewPage(props) {
-  const navigate = useNavigate();
-  const { postId } = useParams();
-
-  const post = data.find((item) => {
-    return item.id == postId;
-  });
+const Index = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalBackground = useRef(null);
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
+    <div className="detail">
+      <div className="button">
+        <p className="arrow"><button><Arrow /></button></p>
+        <p className="setting"><button onClick={() => setModalOpen(true)}><Setting /></button></p>
+      </div>
+
+      {
+        modalOpen && (
+        <div className={'modal-container'} ref={modalBackground} onClick={e => {
+          if (e.target === modalBackground.current) {
+            setModalOpen(false);
+          }
+        }}>
+          <div className={'modal-content'} onClick={e => e.stopPropagation()}>
+            <p className="correction"><button><Correction /></button>수정</p>
+            <p className="delete"><button><Delete /></button>삭제</p>
+          </div>
+        </div>
+      )}
+
+      <div>
+        <textarea className="post"></textarea>
+      </div>
+      <div className="post_title">
+        새로운 직장 첫 출근
+      </div>
+      <div className="post_detail">
+      한글이랑 영어랑 들어가는 느낌이 다르기 때문에 영어로 일기를 쓰지 않는 이상 한글 더미가 들어가는게 더 좋을 것 같습니다. 
+      한글이랑 영어랑 들어가는 느낌이 다르기 때문에 영어로 일기를 쓰지 않는 이상 한글 더미가 들어가는게 더 좋을 것 같습니다.
+      </div>
     </div>
   )
-}
+};
 
-export default index;
+export default Index;
